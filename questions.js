e25eb96df35d1a52e6170048cecc08576dbc2468
harshsuperop <harshgovind18@gmail.com>
2022-11-08 19:50:40 +0530

    finished

---
+++ questions.js
@@ -0,0 +1,168 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What is Ethernet?",
+        answer: "A protocol used by many wired LAN's",
+        options: [
+            "A protocol used by many wired LAN's",
+            "A protocol used for address translation",
+            "A security protocol",
+            "A collision detection protocol"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What is the World Wide Web (www)?",
+        answer: "Collection of webpages stored on websites",
+        options: [
+            "Collection of webpages stored on websites",
+            "Interconnected networks",
+            "Hypertext processor",
+            "Web browser"
+        ]
+    },
+  {
+        numb: 8,
+        question: "Which one of the following is not a search engine?",
+        answer: "Chrome",
+        options: [
+            "Google",
+            "Chrome",
+            "Yahoo",
+            "Bing"
+        ]
+    },
+{
+        numb: 9,
+        question: "What is PHP in programming?",
+        answer: "General purpose programming language",
+        options: [
+            "General purpose programming language",
+            "Central processing unit",
+            "Central processing union",
+            "None of the above"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Which of these is the smallest possible unit of data?",
+        answer: "Bit",
+        options: [
+            "Bit",
+            "Byte",
+            "Kilobyte",
+            "Megabyte"
+        ]
+    },
+    {
+        numb: 11,
+        question: "Which is one of the following is not an output device?",
+        answer: "Keyboard",
+        options: [
+            "Keyboard",
+            "Visual Display Units",
+            "Printer",
+            "Plotter"
+        ]
+    },
+    {
+        numb: 12,
+        question: "Which web browser was developed by Google?",
+        answer: "Chrome",
+        options: [
+            "Internet Explorer",
+            "Firefox",
+            "Safari",
+            "Chrome"
+        ]
+    },
+    {
+        numb: 13,
+        question: "On the web, the base unit for every website or document which contains information is a -",
+        answer: "webpage",
+        options: [
+            "weblog",
+            "webpage",
+            "webserver",
+            "webmail"
+        ]
+    },
+    {
+        numb: 14,
+        question: "IPv6 protocol defines an IP address of",
+        answer: "128 bit",
+        options: [
+            "32 bit",
+            "64 bit",
+            "128 bit",
+            "256 bit"
+        ]
+    },
+    {
+        numb: 15,
+        question: "Who among the following is known as the father of computing?",
+        answer: "Charles Babbage",
+        options: [
+            "Charles Babbage",
+            "Tim Berners Lee",
+            "Philip Don Estridge",
+            "James Gosling"
+        ]
+    }
+];
